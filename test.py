71b2db54aaa692ad70530e5c54270c1f83d92e6f
[Mani Deep] <[manideep2612@gmail.com]>
2024-05-08 19:14:50 +0530

    Now the test is passed for the delimiter as a new line.

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 import unittest
+import re
 class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
+        strnumbers = str(numbers)
+        strnumbers = strnumbers.replace("\n", ",")
+        numbers = strnumbers
+        # if numbers.startswith("//"):
+        #     delimeter, numbers = numbers.split("\n", 1)
+        #     delimeter = delimeter[2:]
+        # numbers_list = re.split(r'[,\\' + delimeter + ']', numbers)
         return sum(int(num) for num in numbers.split(","))
+
+        
 
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
@@ -20,6 +30,11 @@ class TestStringCalculator(unittest.TestCase):
         calculator = StringCalculator()
         result = calculator.add("1,2")
         self.assertEqual(result, 3)
+    
+    def test_newline_seperator(self):
+        calculator = StringCalculator()
+        result = calculator.add("1\n2,3")
+        self.assertEqual(result, 6)
 
 if __name__ == '__main__':
     unittest.main()
