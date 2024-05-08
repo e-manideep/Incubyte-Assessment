bc59c889c785802e9a745bb80f0735e4095797c7
[Mani Deep] <[manideep2612@gmail.com]>
2024-05-08 19:27:50 +0530

    Implemented the functionality  to support custom delimiters and updated the method to raise a ValueError if negative numbers are found.

--- test.py
+++ test.py
@@ -4,14 +4,37 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
+
         strnumbers = str(numbers)
         strnumbers = strnumbers.replace("\n", ",")
         numbers = strnumbers
-        # if numbers.startswith("//"):
-        #     delimeter, numbers = numbers.split("\n", 1)
-        #     delimeter = delimeter[2:]
-        # numbers_list = re.split(r'[,\\' + delimeter + ']', numbers)
-        return sum(int(num) for num in numbers.split(","))
+
+        delimiter = ","
+        if numbers.startswith("//"):
+            delimiter = numbers[2]
+            numbers = numbers[4:]
+            strnumbers = str(numbers)
+            strnumbers = strnumbers.replace("\n", delimiter)
+            numbers = strnumbers
+
+        total = 0
+        negatives = []
+
+        for num in numbers.split(delimiter):
+            try:
+                num = int(num)
+                if num < 0:
+                    negatives.append(str(num))
+                elif num <= 1000:
+                    total += num
+            except ValueError:
+                pass
+
+        if negatives:
+            raise ValueError("Negative numbers not allowed: " + ", ".join(negatives))
+
+        return total
+
 
         
 
@@ -35,6 +58,17 @@ class TestStringCalculator(unittest.TestCase):
         calculator = StringCalculator()
         result = calculator.add("1\n2,3")
         self.assertEqual(result, 6)
+
+    def test_custom_delimiter(self):
+        calculator = StringCalculator()
+        result = calculator.add("//;\n1;2")
+        self.assertEqual(result, 3)
+    
+    def test_negative_numbers(self):
+        calculator = StringCalculator()
+        with self.assertRaises(ValueError) as context:
+            calculator.add("-1,2,-3")
+        self.assertTrue("Negative numbers not allowed: -1, -3" in str(context.exception))
 
 if __name__ == '__main__':
     unittest.main()
